608c2456a51752be1059828d0cff3ae469763378
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2021-12-15 03:23:44 +0200

    Add linters files and fix linters errors

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-    template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   mode: 'development',
